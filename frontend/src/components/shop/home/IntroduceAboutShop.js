6d70f674b5164e4f04236c110f52b1f7735a98e9
Phương Trần Đức <xzonetran@gmail.com>
2024-10-26 16:27:32 +0700

    26-10

--- frontend/src/components/shop/home/IntroduceAboutShop.js
+++ frontend/src/components/shop/home/IntroduceAboutShop.js
@@ -1,26 +1,49 @@
-import React, { Fragment, useContext } from "react";
-import ProductCategoryDropdown from "./ProductCategoryDropdown";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import { HomeContext } from "./index";
+import "./IntroduceAboutShop.css";
 
-const IntroduceAboutShop = (props) => {
+const IntroduceAboutShop = () => {
   const { data, dispatch } = useContext(HomeContext);
+  const [subTitleText, setSubTitleText] = useState("");
+  const subtitle =
+    "𝓣𝓱𝓮 𝓹𝓻𝓸𝓭𝓾𝓬𝓽 𝓲𝓼 𝓬𝓻𝓪𝓯𝓽𝓮𝓭 𝓮𝓷𝓽𝓲𝓻𝓮𝓵𝔂 𝓯𝓻𝓸𝓶 𝓷𝓪𝓽𝓾𝓻𝓪𝓵 𝓶𝓪𝓽𝓮𝓻𝓲𝓪𝓵𝓼, 𝓭𝓮𝓼𝓲𝓰𝓷𝓮𝓭 𝓽𝓸 𝓫𝓮 𝓫𝓸𝓽𝓱 𝓾𝓼𝓮𝓻-𝓯𝓻𝓲𝓮𝓷𝓭𝓵𝔂 𝓪𝓷𝓭 𝓱𝓲𝓰𝓱𝓵𝔂 𝓮𝓯𝓯𝓮𝓬𝓽𝓲𝓿𝓮... 𝓮𝓷𝓼𝓾𝓻𝓲𝓷𝓰 𝓪 𝓬𝓸𝓶𝓯𝓸𝓻𝓽𝓪𝓫𝓵𝓮 𝓪𝓷𝓭 𝓮𝓷𝓳𝓸𝔂𝓪𝓫𝓵𝓮 𝓸𝓾𝓽𝓭𝓸𝓸𝓻 𝓮𝔁𝓹𝓮𝓻𝓲𝓮𝓷𝓬𝓮.";
+
+  useEffect(() => {
+    let index = 0;
+    const typingInterval = setInterval(() => {
+      if (index < subtitle.length) {
+        setSubTitleText((prev) => prev + subtitle[index]);
+        index++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 30);
+
+    return () => clearInterval(typingInterval);
+  }, []);
 
   return (
     <Fragment>
-      <section className="">
-        <div className=" about-shop">
-          <h3 className="hearder-about-shop text-center">
+      <section className="introduce-section relative">
+        {/* Background Video */}
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        >
+          <source src="./image/rain.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        {/* Content */}
+        <div className="about-shop relative z-10 p-6 rounded-lg shadow-lg bg-white bg-opacity-80">
+          <h3 className="header-about-shop text-center text-3xl font-bold">
             𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐎𝐔𝐑 𝐒𝐓𝐎𝐑𝐄
           </h3>
-          <div className="sub-title-about-shop text-center mt-2 mb-2">
-          𝓣𝓱𝓮 𝓹𝓻𝓸𝓭𝓾𝓬𝓽 𝓲𝓼 𝓬𝓻𝓪𝓯𝓽𝓮𝓭 𝓮𝓷𝓽𝓲𝓻𝓮𝓵𝔂 𝓯𝓻𝓸𝓶 𝓷𝓪𝓽𝓾𝓻𝓪𝓵 𝓶𝓪𝓽𝓮𝓻𝓲𝓪𝓵𝓼, 
-          <br/>
-          𝓭𝓮𝓼𝓲𝓰𝓷𝓮𝓭 𝓽𝓸 𝓫𝓮 𝓫𝓸𝓽𝓱 𝓾𝓼𝓮𝓻-𝓯𝓻𝓲𝓮𝓷𝓭𝓵𝔂 𝓪𝓷𝓭 𝓱𝓲𝓰𝓱𝓵𝔂 𝓮𝓯𝓯𝓮𝓬𝓽𝓲𝓿𝓮 𝓲𝓷 𝓹𝓻𝓸𝓿𝓲𝓭𝓲𝓷𝓰 𝓹𝓻𝓸𝓽𝓮𝓬𝓽𝓲𝓸𝓷 𝓪𝓰𝓪𝓲𝓷𝓼𝓽 𝓼𝓾𝓷 𝓪𝓷𝓭 𝓻𝓪𝓲𝓷, 
-          <br/>
-          𝓮𝓷𝓼𝓾𝓻𝓲𝓷𝓰 𝓪 𝓬𝓸𝓶𝓯𝓸𝓻𝓽𝓪𝓫𝓵𝓮 𝓪𝓷𝓭 𝓮𝓷𝓳𝓸𝔂𝓪𝓫𝓵𝓮 𝓸𝓾𝓽𝓭𝓸𝓸𝓻 𝓮𝔁𝓹𝓮𝓻𝓲𝓮𝓷𝓬𝓮.
-          <br/>
+          <div className="sub-title-about-shop text-center mt-4 mb-4">
+            {subTitleText}
           </div>
-          {/*  */}
         </div>
       </section>
     </Fragment>
--- frontend/src/components/shop/home/ProductCategory.js
+++ frontend/src/components/shop/home/ProductCategory.js
@@ -1,6 +1,7 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import ProductCategoryDropdown from "./ProductCategoryDropdown";
 import { HomeContext } from "./index";
+import "./style.css"; 
 
 const ProductCategory = (props) => {
   const images = [
@@ -11,15 +12,26 @@ const ProductCategory = (props) => {
   ];
   const { data, dispatch } = useContext(HomeContext);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const items = document.querySelectorAll(".fade-in");
+      items.forEach((item) => item.classList.add("visible"));
+    }, 100); // Adds fade-in delay
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Fragment>
-      <div className="py-1 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+      <div className="py-1 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {images.map((img, index) => (
-          <div className="col-span-1 m-2 flex flex-col items-center justify-center space-y-2 cursor-pointer border shadow-lg rounded  ">
+          <div
+            key={index}
+            className="col-span-1 m-2 flex flex-col items-center justify-center space-y-2 cursor-pointer border shadow-lg rounded fade-in"
+          >
             <img
-              className="fix-image-categories object-center cursor-pointer  rounded-top image-intro"
-              key={index}
+              className="fix-image-categories object-center cursor-pointer rounded-top image-intro scale-hover"
               src={img}
+              alt={`Product ${index}`}
             />
           </div>
         ))}
